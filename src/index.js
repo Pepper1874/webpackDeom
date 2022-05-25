184545d4193ae29685128b45394551462dc8cecf
Pepper Li <15761636783@163.com>
2022-05-25 16:43:58 +0800

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import logo from './img.jpg';
+import data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['你好', '世界'], ' ');
+    element.classList.add('hello');
+
+    const img = new Image();
+    img.src = logo;
+
+    element.appendChild(img);
+
+    console.log(data);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,34 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.css$/, // 正则匹配 css 文件
+            use: [ // 使用以下 loader 处理 css 文件
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.(png|gif|svg|jpg)$/, // 正则匹配图片文件
+            use: [ // 使用以下 loader 处理图片文件
+                'file-loader'
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/, // 正则匹配字体文件
+            use: [ // 使用以下 loader 处理字体文件
+                'file-loader'
+            ]
+        }, {
+            test: /\.(csv|tsv)$/,
+            use: [
+                'csv-loader'
+            ]
+        }, {
+            test: /\.xml$/,
+            use: [
+                'xml-loader'
+            ]
+        }]
     }
 }
