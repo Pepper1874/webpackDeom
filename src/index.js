b6d8c6a5cfcc560738039cde9b21918895a2699a
Pepper Li <15761636783@163.com>
2022-05-27 10:07:42 +0800

    DevServer and HMR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
@@ -14,4 +15,15 @@ function component() {
     return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 当页面重新渲染时重新获取元素
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', () => {
+        console.log('Accepting the updated print module !');
+        // printMe();
+        document.body.removeChild(element);
+        element = component(); // 当  print.js 改变后重新绑定最新的回调
+        document.body.appendChild(element);
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require(webpack);
+
 module.exports = {
     mode: 'development',
     entry: {
         app: path.resolve(__dirname, 'src', 'index.js'),
-        print: path.resolve(__dirname, 'src', 'print.js'),
+        // print: path.resolve(__dirname, 'src', 'print.js'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3000,
+        // contentBase: path.resolve(__dirname, 'dist'),
+        static: path.resolve(__dirname, 'dist'),
+        hot: true, // 开启热模块替换
     },
     plugins: [
         new CleanWebpackPlugin(), // 动态清除 dist 下无用的生成文件, 可扩展配置需要传入一个对象
         new HtmlWebpackPlugin({ // 动态的生成最终的 HTML 文件
             title: 'Output Management',
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModulesReplacementPlugin()
     ],
     module: {
         rules: [{
